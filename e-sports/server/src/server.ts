5da96827696dcfbfdbadea692215bc6f4e3f17ea
Andressa Da Costa <andressadacostan@gmail.com>
2022-09-15 22:13:51 -0300

    back-end-server

--- e-sports/server/src/server.ts
+++ e-sports/server/src/server.ts
@@ -11,7 +11,9 @@ app.get('/ads', (request, response) => {
 	return response.json([
 		{ id: 1, name: 'Anúncio 1' },
 		{ id: 2, name: 'Anúncio 2' },
-		{ id: 3, name: 'Anúncio 3' }
+		{ id: 3, name: 'Anúncio 3' },
+		{ id: 4, name: 'Anúncio 4' }
+
 	]);
 });
 
